fe525ff14f3418811137c5d1e1c6e3ae4ea37cf3
Filipe Laíns <lains@riseup.net>
2021-12-31 17:30:23 +0000

    plugins: implement subcommand plugin
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- conda/plugins.py
+++ conda/plugins.py
@@ -6,3 +6,26 @@ import pluggy
 
 _hookspec = pluggy.HookspecMarker('conda')
 hookimp = pluggy.HookimplMarker('conda')
+
+
+class CondaSubcommand(NamedTuple):
+    """Conda subcommand entry.
+
+    :param name: Subcommand name (as-in ``conda my-subcommand-name``).
+    :param summary: Subcommand summary, will be shown in ``conda --help``.
+    :param action: Callable that will be run when the subcommand is invoked.
+    """
+    name: str
+    summary: str
+    action: Callable[
+        [List[str]],  # arguments
+        Optional[int],  # return code
+    ]
+
+
+@_hookspec
+def conda_cli_register_subcommands() -> Iterable[CondaSubcommand]:
+    """Register external subcommands in Conda.
+
+    :return: An iterable of subcommand entries.
+    """
